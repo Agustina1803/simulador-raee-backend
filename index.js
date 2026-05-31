0fa322fa4e2464a1b22ab08aac09db5a880302ac
AgusBulacio <agusbulacio1803@gmail.com>
2026-05-31 19:34:17 -0300

    agregue la configuracion de las rutas

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Server from './server.js';
 import router from './routes/index.routes.js';  
 
 const server = new Server();
-server.app.use('/api', router);
+server.app.use('/', router);
 
 
 //server.listen();
