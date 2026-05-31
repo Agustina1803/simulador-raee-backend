5666d0c5a36e8cba9966c1e2543b712f6785d34a
AgusBulacio <agusbulacio1803@gmail.com>
2026-05-31 19:09:12 -0300

    configure cords para que pueda acceder el front

--- server.js
+++ server.js
@@ -9,44 +9,66 @@ export default class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 3000;
+
+   
+    this.connectDatabase();
+
+   
     this.middlewares();
+
+   
+    this.routes();
+  }
+
+  async connectDatabase() {
+    try {
+      await connectDB();
+      console.log("✅ Conectado a la base de datos");
+    } catch (error) {
+      console.error("❌ Error al conectar DB:", error);
+      process.exit(1); 
+    }
   }
 
   middlewares() {
-    this.app.use(async (req, res, next) => {
-      try {
-        await connectDB();
-        next();
-      } catch (error) {
-        console.error("Error connecting to DB:", error);
-        res.status(500).json({ message: "Database connection error" });
-      }
-    });
+    
+    const allowedOrigins = [
+      "http://localhost:3000", 
+      "https://simuladorraee2026.netlify.app" 
+    ];
 
     this.app.use(
       cors({
-        origin: true,
+        origin: allowedOrigins,
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
       })
     );
+
+   
     this.app.use(morgan("dev"));
+
+    
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
+
+   
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    console.log(__filename);
-    console.log(__dirname);
     this.app.use(express.static(path.join(__dirname, "../public")));
+  }
 
+  routes() {
     this.app.get("/", (req, res) => {
-      res.send("Backend simulador funcionando");
+      res.send("Backend simulador funcionando 🚀");
     });
+
+   this.app.use("/simulacion", simulacionRouter);
   }
 
   listen() {
     this.app.listen(this.port, () => {
-      console.info(`server inciiado: http://127.0.0.1:${this.port}`);
+      console.info(`✅ Servidor iniciado en http://127.0.0.1:${this.port}`);
     });
   }
 }
